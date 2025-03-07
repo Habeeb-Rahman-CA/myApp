92c0b32adf1c78f7e999e6a1dc43147520188736
Habeeb Rahman <habeebrahmanca07@gmail.com>
2025-03-07 16:03:35 +0530

    field, setter, constructor injection

--- src/main/java/com/habeeb/myApp/Dev.java
+++ src/main/java/com/habeeb/myApp/Dev.java
@@ -1,10 +1,25 @@
 package com.habeeb.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
-    public void build(){
+
+    @Autowired // Connect the Laptop.java to Dev.java (Field Injection)
+    private Laptop laptop;
+
+    // public Dev(Laptop laptop) { // Another way to connect laptop to dev (Constructor Injection)
+    //     this.laptop = laptop;
+    // }
+
+    @Autowired
+    public void setLaptop(Laptop laptop){ // Another way of DI (Setter Injection)
+        this.laptop = laptop;
+    }
+
+    public void build() {
+        laptop.compile();
         System.out.println("Working on Java Project");
     }
 }
